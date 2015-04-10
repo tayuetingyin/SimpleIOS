ce5ebb8a0a4f331966644c30e96792715b7af01e
刘旭 <Ksy@lixudeAir.lan>
2015-04-10 16:34:52 +0800

    demo

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 刘旭 on 15/4/10.
+//  Copyright (c) 2015年 liuxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 刘旭 on 15/4/10.
+//  Copyright (c) 2015年 liuxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
